d65e21ea52e09cc35b38d0c55b5ff789b2eac8cf
Germán Rojas <g.rojas@uniandes.edu.co>
2025-03-19 17:14:37 -0500

    Add navigation setup in MainActivity for login, registration, and home screens

--- app/src/main/java/com/example/unimarket/MainActivity.kt
+++ app/src/main/java/com/example/unimarket/MainActivity.kt
@@ -3,14 +3,14 @@ package com.example.unimarket
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.unimarket.ui.home.HomeScreen
+import com.example.unimarket.ui.login.LoginScreen
+import com.example.unimarket.ui.register.RegisterScreen
 import com.example.unimarket.ui.theme.UniMarketTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,11 +18,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             UniMarketTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold {
+                    AppNavigation()
                 }
             }
         }
@@ -30,17 +27,39 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun AppNavigation() {
+    val navController = rememberNavController()
+    NavHost(
+        navController = navController,
+        startDestination = "login"  // First screen is login
+    ) {
+        composable("login") {
+            LoginScreen(
+                onLoginSuccess = {
+                    // If login successful, go to Home
+                    navController.navigate("home") {
+                        // Avoid user to go back
+                        popUpTo("login") { inclusive = true }
+                    }
+                },
+                onNavigateToRegister = {
+                    // Navigate register screen
+                    navController.navigate("register")
+                }
+            )
+        }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    UniMarketTheme {
-        Greeting("Android")
+        composable("register") {
+            RegisterScreen(
+                onRegisterSuccess = {
+                    // After registration, go back to login screen
+                    navController.popBackStack()
+                }
+            )
+        }
+
+        composable("home") {
+            HomeScreen()
+        }
     }
 }
