b99d0863207a7fe8b666e5c2ae85064f81d0b4e0
Germán Rojas <g.rojas@uniandes.edu.co>
2025-03-21 17:16:38 -0500

    REFINE: Add Firebase Performance Monitoring dependency in build configuration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,4 +69,5 @@ dependencies {
     implementation(libs.androidx.material.icons.extended)
     implementation(libs.firebase.crashlytics)
     implementation(libs.firebase.analytics.ktx)
+    implementation(libs.firebase.perf)
 }
