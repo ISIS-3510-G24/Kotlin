06486d609bddc19ad29023f8ab023bc435d3f6c8
Germán Rojas <g.rojas@uniandes.edu.co>
2025-03-21 17:00:09 -0500

    REFINE: Integrate Firebase Analytics for app open event logging in MainActivity

--- app/src/main/java/com/example/unimarket/MainActivity.kt
+++ app/src/main/java/com/example/unimarket/MainActivity.kt
@@ -19,12 +19,16 @@ import com.example.unimarket.ui.onboarding.OnboardingScreen
 import com.example.unimarket.ui.onboarding.PersonalizationScreen
 import com.example.unimarket.ui.register.RegisterScreen
 import com.example.unimarket.ui.theme.UniMarketTheme
+import com.google.firebase.analytics.FirebaseAnalytics
 import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : ComponentActivity() {
     @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        FirebaseAnalytics.getInstance(this).logEvent("app_open", Bundle())
+
         setContent {
             UniMarketTheme {
                 Scaffold {
