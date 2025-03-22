612d6563058e171418b9378f5ddb22ae08b9e81d
Germán Rojas <g.rojas@uniandes.edu.co>
2025-03-21 19:56:57 -0500

    REFINE: Remove linting step from predeploy in firebase.json and standardize string quotes in index.ts

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,30 +1,30 @@
-import { onDocumentCreated } from 'firebase-functions/v2/firestore';
-import * as admin from 'firebase-admin';
+import {onDocumentCreated} from "firebase-functions/v2/firestore";
+import * as admin from "firebase-admin";
 
 admin.initializeApp();
 
 export const sendProductNotification = onDocumentCreated(
-  { document: 'Product/{productId}' },
+  {document: "Product/{productId}"},
   async (event) => {
     const product = event.data?.data();
     if (!product || !product.title) {
-      console.error('Product data is missing or title is undefined.');
+      console.error("Product data is missing or title is undefined.");
       return;
     }
 
     const payload: admin.messaging.MessagingPayload = {
       notification: {
-        title: 'New Product Published!',
+        title: "New Product Published!",
         body: `Check out the product: ${product.title}`,
-        click_action: 'KOTLIN_NOTIFICATION_CLICK' // This is the action that will be triggered when the notification is clicked
+        click_action: "KOTLIN_NOTIFICATION_CLICK" // This is the action that will be triggered when the notification is clicked
       }
     };
 
     try {
-      const response = await admin.messaging().sendToTopic('new_products', payload);
-      console.log('Notification sent successfully:', response);
+      const response = await admin.messaging().sendToTopic("new_products", payload);
+      console.log("Notification sent successfully:", response);
     } catch (error) {
-      console.error('Error sending notification:', error);
+      console.error("Error sending notification:", error);
     }
   }
 );
