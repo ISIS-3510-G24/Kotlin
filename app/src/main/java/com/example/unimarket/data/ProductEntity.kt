ac10297a0c63f04a568444302c07fe809c46924d
Germán Rojas <g.rojas@uniandes.edu.co>
2025-04-23 14:44:46 -0500

    REFINE: Create ProductEntity data class for Room database integration

---
+++ app/src/main/java/com/example/unimarket/data/ProductEntity.kt
@@ -0,0 +1,16 @@
+package com.example.unimarket.data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "products")
+data class ProductEntity(
+    @PrimaryKey val id: String,
+    val title : String,
+    val description: String,
+    val price: Double,
+    val imageUrls: List<String>,
+    val labels: List<String>,
+    val status: String,
+    val fetchedAt: Long
+)
