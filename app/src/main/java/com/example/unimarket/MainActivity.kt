165d173d7745a9dbb4f851517d751df4990aed81
Germán Rojas <g.rojas@uniandes.edu.co>
2025-05-24 00:52:25 -0500

    REFINE: Remove unused import for Scaffold in MainActivity to improve code cleanliness

--- app/src/main/java/com/example/unimarket/MainActivity.kt
+++ app/src/main/java/com/example/unimarket/MainActivity.kt
@@ -6,7 +6,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.RequiresApi
-import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
